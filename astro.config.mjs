4701127c26336c25f23add3cce99c32443c203d6
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-01 20:49:40 +0530

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   devToolbar: {
     enabled: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <div className="text-3xl font-bold">Stabldev</div>;
+}
