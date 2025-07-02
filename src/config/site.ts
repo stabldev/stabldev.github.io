6c3051cdefb02d1d0fea4b9f42d87568ed95028c
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-02 10:30:54 +0530

    feat: siteConfig

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  links: [
+    {
+      href: "https://github.com/stabldev",
+      iconName: "github",
+      iconClass: "size-4.5",
+      text: "GitHub",
+    },
+    {
+      href: "https://x.com/stabldev",
+      iconName: "x",
+      iconClass: "size-4",
+      text: "X",
+    },
+    {
+      href: "/rss.xml",
+      iconName: "rss",
+      iconClass: "size-3.5",
+      text: "RSS",
+    },
+  ],
+};
