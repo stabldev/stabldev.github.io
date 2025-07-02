cf4c2fc9dda3ac4ae736c45f6a106c8c9589f532
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-02 16:55:01 +0530

    chore: update content

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob, file } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/**.md", base: "./content/blog" }),
+  loader: glob({ pattern: "**/**.md", base: "./content" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
