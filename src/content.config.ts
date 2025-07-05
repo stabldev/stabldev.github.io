6d583399ad8c58221af8d50b0aac7cb65cf4cb58
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-05 07:49:16 +0530

    fix: filter toc headings upto depth 3 (#3)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/**.{md,mdx}", base: "./content/p" }),
