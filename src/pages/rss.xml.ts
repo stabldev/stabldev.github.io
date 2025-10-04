2ce58967f2989e902f9c0062a7a568600766d724
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-04 19:43:16 +0530

    chore: remove prettier

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,15 +3,16 @@ import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ site }) => {
-	const posts = await getCollection("posts");
+  const posts = await getCollection("posts");
 
-	return rss({
-		title: "Stabldev’s Blog",
-		description: "A place where I write, build, and share all things dev.",
-		site: site!,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/p/${post.id}/`,
-		})),
-	});
+  return rss({
+    title: "Stabldev’s Blog",
+    description: "A place where I write, build, and share all things dev.",
+    // @ts-expect-error: site is always valid URL
+    site: site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/p/${post.id}/`,
+    })),
+  });
 };
